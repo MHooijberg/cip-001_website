9a87b326b64d050fa50b78f54a975e90d44470cf
MHooijberg <m.hooijberg24@gmail.com>
2025-10-11 15:01:10 +0200

    Finished contact page, using environment variables, and development logs integrated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   build: {
     format: 'preserve',
+  },
+  env: {
+    schema: {
+      PUBLIC_AWS_API_GATEWAY_ENDPOINT: envField.string({ context: "client", access: "public", optional: false }),
+    }
   },
   output: "static",
   vite: {
